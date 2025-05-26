8d9e71c5a0082a7c0c402bf2c73b47377fb62329
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-05-26 19:32:17 -0300

    feat: cria o component list-sales

---
+++ SalesAndInventoryDashboard-FE/src/app/components/list-sales/list-sales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-sales',
+  imports: [],
+  templateUrl: './list-sales.component.html',
+  styleUrl: './list-sales.component.css'
+})
+export class ListSalesComponent {
+
+}
