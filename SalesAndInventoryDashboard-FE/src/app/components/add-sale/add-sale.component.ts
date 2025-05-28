2e528a06dd44111eb0ca5bd52a70a847f23ce271
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-05-28 20:26:09 -0300

    feat(add-sale): adiciona o component

---
+++ SalesAndInventoryDashboard-FE/src/app/components/add-sale/add-sale.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-sale',
+  imports: [],
+  templateUrl: './add-sale.component.html',
+  styleUrl: './add-sale.component.css'
+})
+export class AddSaleComponent {
+
+}
