8e696963f1fe5bfc125bbc5db279d8c76c47394e
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-05-29 20:30:03 -0300

    feat: cria o add-sale e estiliza

--- SalesAndInventoryDashboard-FE/src/app/app.routes.ts
+++ SalesAndInventoryDashboard-FE/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ListProductsComponent } from './components/list-products/list-products.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { ListSalesComponent } from './components/list-sales/list-sales.component';
+import { AddSaleComponent } from './components/add-sale/add-sale.component';
 
 export const routes: Routes = [
     {
@@ -17,5 +18,10 @@ export const routes: Routes = [
      {
         path: 'list-sales',
         component: ListSalesComponent,
+    },
+
+    {
+        path: 'add-sales',
+        component: AddSaleComponent,
     }
 ];
--- SalesAndInventoryDashboard-FE/src/app/components/add-sale/add-sale.component.ts
+++ SalesAndInventoryDashboard-FE/src/app/components/add-sale/add-sale.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-sale',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-sale.component.html',
   styleUrl: './add-sale.component.css'
 })
